0168822f9fb630e0215315bc2ee7cc1390b674f0
Dhrupal Kamlesh Desai <desai.dhrupal15@gmail.com>
2022-02-06 08:05:27 -0500

    Initial Commit

---
+++ Lab2_8805252/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2_8805252
+//
+//  Created by user204862 on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
